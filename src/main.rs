99355426cac7a9e51adbd4b72536e23ea017c41b
krzych85 <krzdudek@gmail.com>
2019-11-19 22:49:44 +0100

    first function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::ops::Add;
+
+fn sum_numbers<T>(a: T, b: T) -> T
+where T: Add<Output=T> + Copy + Clone{
+  let result = a + b;
+  result
+}
+
 fn main() {
-    println!("Hello, hello!");
+    let sum = sum_numbers(2,5);
+    println!("{}", sum);
 }
